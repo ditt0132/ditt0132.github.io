06ecf2cb17efbc196e58ded5bbd9a106b17b9324
plz-Enter-name <104212145+plz-Enter-name@users.noreply.github.com>
2022-06-16 21:38:00 +0900

    이스터에그 더 추가

--- runner/scripts/egg.js
+++ runner/scripts/egg.js
@@ -2,6 +2,7 @@ var eggnum = 0;
 document.getElementById("egg").addEventListener("click", listner => {
     eggnum = (eggnum + 1);
     if (eggnum === 20) {
-        document.getElementById("botegg").disabled = true
+        document.getElementById("botegg").disabled = true;
+        document.getElementById("botegg").value = "º¿À» È°¼ºÈ­?";
     }
 });
