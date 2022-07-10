dd987d890405a25edb8e6805dae3fbd898bbff73
plz-Enter-name <104212145+plz-Enter-name@users.noreply.github.com>
2022-07-10 16:20:03 +0900

    ㅁㄴㅇㄹ

---
+++ t.js
@@ -0,0 +1,6 @@
+document.getElementById("tolang").innerHTML = Hanguel.d(document.getElementById("kolang").value).toString().replace(/ /g,">").replace(/,/g,"").replace(/[/g,"").replce(/]/g," "),true)
+  .replace(/ㄱ/g,"GT")
+  .replace(/ㄲ/g,"GT\'")
+  .replace(/ㄴ/g,"NT")
+  .replace
+           
