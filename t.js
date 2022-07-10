611fc11acd4acdecc57bbfc320047f9ac31b799e
plz-Enter-name <104212145+plz-Enter-name@users.noreply.github.com>
2022-07-10 16:22:18 +0900

    ㅁㄴㅇ

--- t.js
+++ t.js
@@ -1,6 +1,28 @@
-document.getElementById("tolang").innerHTML = Hanguel.d(document.getElementById("kolang").value).toString().replace(/ /g,">").replace(/,/g,"").replace(/[/g,"").replce(/]/g," "),true)
-  .replace(/ㄱ/g,"GT")
-  .replace(/ㄲ/g,"GT\'")
-  .replace(/ㄴ/g,"NT")
-  .replace
-           
+function getKor(kor) {
+    const f = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ',
+               'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ',
+               'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
+    const s = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ',
+               'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ',
+               'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ'];
+    const t = ['', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ',
+               'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ',
+               'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ',
+               'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
+
+    const ga = 44032;
+    let uni = kor.charCodeAt(0);
+
+    uni = uni - ga;
+
+    let fn = parseInt(uni / 588);
+    let sn = parseInt((uni - (fn * 588)) / 28);
+    let tn = parseInt(uni % 28);
+
+    return {
+        f: f[fn],
+        s: s[sn],
+        t: t[tn]
+    };
+}
+console.log(getKor("안녕")
